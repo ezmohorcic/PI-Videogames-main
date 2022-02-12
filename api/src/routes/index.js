bae87a1ecbfdf872679633eeb49d54fb21be61cd
ezmohorcic <ezmohorcic@gmail.com>
2022-02-11 22:00:22 -0300

    draft 22.00 11/02/22

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const res = require('express/lib/response');
+const YOUR_API_KEY='0d0560168f704159886770370807e888';
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const { Videogame,genre } = require('../db.js');
@@ -8,25 +9,38 @@ const router = Router();
 
 async function getVideogames(name,page)
 {
-    
+    let raw=Videogame.findAll({limit:page*15,where:{name:'%'+name+'%'}}); //busco los juegos en db
+    if(raw.length<page*15){raw=[];} //si hay menos que la pagina, es que mostre todos
+    else{raw=raw.slice((page*15)-1,(page+1)*15)} //sino no mostre todos, elimino los que ya mostre
+    if(raw.length==15){return raw}
+    let apiRaw = await fetch(`https://api.rawg.io/api/games/?search=${name}&key=${YOUR_API_KEY}`) //fetcheo a api
+    if(raw.length>0) //si hay juegos de db, significa que es la primera vez que muestro juegos de api
+    {
+        apiRaw=apiRaw.slice(0,15-raw.length); //busco para rellenar 15
+        return raw.concat(apiRaw); //concateno y devuelvo
+    }
+    else
+    {
+        return apiRaw.slice((page*15)-1,(page+1)*15)  //corto de atras por cantidad de paginas, y adelante para dejar 15 
+    }
 }
 
 async function getVideogameByID(idVideogame)
 {
     if(typeof idVideogame =="number") //es de api
     {
-        const resp = await fetch(`https://api.rawg.io/api/games/${idVideogame}?key=${YOUR_API_KEY}`)
+        const resp = await fetch(`https://api.rawg.io/api/games/${idVideogame}?key=${YOUR_API_KEY}`);
         const raw=await resp.json();
         const out= await raw.map((element,index)=>
         {
             return{
                 ID:element.results.id,
-                name:element.results.name,
-                description:element.results.reviews_text_count,
-                releaseDate:element.results.released,
-                rating:element.results.rating,
-                platforms:results.results.platforms,//ver como funciona
-                background_image:element.results.background_image,
+                name:element.results[0].name,
+                description:element.results[0].reviews_text_count,
+                releaseDate:element.results[0].released,
+                rating:element.results[0].rating,
+                platforms:results.results[0].platforms,//ver como funciona
+                background_image:element.results[0].background_image,
             }
         });
         return out;
