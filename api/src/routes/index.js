b03afc7ea8db3087d4f5dab7eb5e4e71cb0c6737
ezmohorcic <ezmohorcic@gmail.com>
2022-02-13 03:24:49 -0300

    Ordenamiento funcionando, filtro en db falla

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -35,6 +35,28 @@ async function reSize(page,out,apiRaw,dbRaw,name)
     }
 }
 
+function compareName( a, b ) {
+    //console.log(a.name)
+    //console.log(b.name)
+    if ( a.name < b.name ){
+      return -1;
+    }
+    if ( a.name > b.name ){
+      return 1;
+    }
+    return 0;
+  }
+
+function compareRatings( a, b ) {
+  if ( a.rating > b.rating ){
+    return -1;
+  }
+  if ( a.rating < b.rating ){
+    return 1;
+  }
+  return 0;
+}
+
 async function getVideogames(name,page=0,filter,order)
 {
     //--me traigo todo de la db y api--
@@ -72,13 +94,14 @@ async function ALTERgetVideogames(name,page=0,filter,order)
     if(name){dbRaw=dbRaw.filter(vdg => vdg.name.toLowerCase().includes(name.toLowerCase()));}//filtro por nombre en base de datos}
 
     var apiRaw =[];
-    for(let i=0;i<6;i++) //Traigo 100 juegos por indicado en ReadMe
+    for(let i=1;i<6;i++) //Traigo 100 juegos por indicado en ReadMe
     {
-        let temp;
-        if(name){ temp = await fetch(`https://api.rawg.io/api/games?search=${name}&key=${api_key}`);} //si hay que filtrar por nombre
-        else {temp= await fetch(`https://api.rawg.io/api/games?key=${api_key}`);}   //si no hay que filtrar por nombre
-        temp = await temp.json;
-        temp=temp.result;
+        let temp={};
+        if(name){ temp = await fetch(`https://api.rawg.io/api/games?search=${name}&key=${api_key}&page=${i}`);} //si hay que filtrar por nombre
+        else {temp= await fetch(`https://api.rawg.io/api/games?key=${api_key}&page=${i}`);}   //si no hay que filtrar por nombre
+        temp = await temp.json();
+        temp=temp.results;
+        if(!temp){temp=[];}
         apiRaw=[...apiRaw,...temp];
     }
 
@@ -88,12 +111,25 @@ async function ALTERgetVideogames(name,page=0,filter,order)
     {
         switch (filter.type) {
             case "genero":
-                dbRaw=dbRaw.filter(vd => vd.genres.includes(filter.payload));
-                apiRaw
+                dbRaw=dbRaw.filter(vd => vd.genres.includes(filter.payload)); //revisar esto, ver como filtrar DB
+                apiRaw = apiRaw.filter(vd=>
+                    {
+                        let flag=false;
+                        console.log(vd.name)
+                        vd.genres.forEach(element => 
+                        {
+                            if(element.name==filter.payload)
+                            {
+                                flag=true;
+                            }
+                        });
+                        return flag;
+                    });
             break;
         
             case "dbOapi":
-
+                    if(filter.payload=="db"){apiRaw=[];}
+                    else if(filter.payload=="api"){dbRaw=[];}
             break;
 
             default:
@@ -102,22 +138,33 @@ async function ALTERgetVideogames(name,page=0,filter,order)
     }
 
     let outRaw = [...dbRaw,...apiRaw];
+    let logg= outRaw.map(vg=>vg.name);
+    //console.log(logg)
 
-    if(order)// alfabetico || rating
+    if(order) // alfabetico || rating
     {
         switch (order.type) {
             case 'alfabetico':
-                
+                outRaw.sort(function(a,b)
+                {
+                    if(a.name < b.name){return -1;}
+                    if(a.name > b.name){return 1;}
+                    return 0;
+                });
             break;
 
             case 'rating':
-                
+                outRaw.sort((a,b)=>b.rating-a.rating);
             break;
         
             default:
             break;
         }
     }
+
+
+    let out = outRaw.slice(page*15,(page+1)*15);// || page=0 => 0*15 a (1*15)-1 == 0 a 14 || page=1 => 1*15 a ((1+1)*15)-1 == 15 a 29 || 
+    return out;
 }
 
 async function getVideogameByID(idVideogame)
@@ -201,7 +248,7 @@ router.get('/videogames',async function(req,res)
     {
         let {name}=req.query;
         let {page,filter,order}=req.body
-        let videogames = await getVideogames(name,page,filter,order);
+        let videogames = await ALTERgetVideogames(name,page,filter,order);
         return res.json(videogames);
     }
     catch(e){console.log(e);}
