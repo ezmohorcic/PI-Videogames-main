22e5c24b6796ebfae4b570f89d599f4284858300
ezmohorcic <ezmohorcic@gmail.com>
2022-02-18 18:25:15 -0300

    css genres listo

--- client/src/components/ShowGenres/ShowGenres.jsx
+++ client/src/components/ShowGenres/ShowGenres.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
-import { getAllGenres } from '../../redux/actions';
+import { getAllGenres, setFilter, setVideogamesPorBuscando } from '../../redux/actions';
+import { Link } from 'react-router-dom';
 
-export function ShowGenres(props)
+import './ShowGenres.css'
+
+export function ShowGenres()
 {
     const genres = useSelector((state)=>state.genres);
     const dispatch=useDispatch();
 
     useEffect(()=>{if(genres.length===0){dispatch(getAllGenres())}},[])
 
+    function handleGenreShowAll(e,element){
+        console.log(element)
+        console.log(e)
+        dispatch(setVideogamesPorBuscando());
+        dispatch(setFilter({type:"genero",payload:element.name}));
+    }
+
     let arrGenres=genres.map((element,index)=>
     {
         return(
-            <div className="genreButCont" key={"genre"+index}>{element.name}</div>
+            <div className="genreButCont" key={"genre"+index}><Link to={"/ShowAll"} className="genreButLink" onClick={(e)=>{handleGenreShowAll(e,element)}}>{element.name}</Link></div>
         )
     });
     return(
         <div id='showGenresContainer'>
-                {arrGenres}
+            <h1 id='genresh1'>Genres owo!</h1>
+            <div id='showGenresShell'>{arrGenres}</div>
         </div>
     )
 }
